a224e8170374ad55a055607ab9ea2c47475e34be
Abhishek Gupta <83904358+Abhishek-gupta-1911@users.noreply.github.com>
2023-03-28 10:58:28 +0530

    added armstrong number program

--- README.md
+++ README.md
@@ -16,5 +16,6 @@ In case, the list becomes too big in the future you can use `cmd + f` or `ctrl +
   - [Ouick Sort](./sorting/Quick_sort.c)
   - [Selection Sort](./sorting/selectionsort.c)
 - [Other Programs](./otherprograms)
+  - [Armstrong number or not](./otherprograms/armstrongnumber.c)
   - [Prime number or not](./otherprograms/primenumber.c)
   - [Prime numbers upto n](./otherprograms/primenumupton.c)
---
+++ otherprograms/armstrongnumber.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_armstrong_number(int num) {
+    int n = num;
+    int r, x, i;
+    int a = 0, sum_of_digits = 0;
+    while (n > 0) {
+        n /= 10;
+        a++;
+    }
+    n = num;
+    while (n > 0) {
+        r = n % 10;
+        x = 1;
+        for(i=0; i<a; i++)
+            x *= r;
+        sum_of_digits += x;
+        n = n / 10;
+    }
+    if (sum_of_digits == num)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int armstrong_num = is_armstrong_number(n);
+    if (armstrong_num)
+        printf("Yes, number %d is an armstrong number", n);
+    else
+        printf("No, number %d is not an armstrong number", n);
+    return 0;
+}
